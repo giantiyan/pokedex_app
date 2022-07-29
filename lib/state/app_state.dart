929718f504f5ee31ac69e4a6b98cf50a7686c2df
giantiyan <61373410+giantiyan@users.noreply.github.com>
2022-07-29 16:37:49 +0800

    [PKX-002] Set up async_redux structure

---
+++ lib/state/app_state.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pokedex/api/models/pokemon_model.dart';
+import 'package:pokedex/api/models/pokemon_type_model.dart';
+import 'package:pokedex/api/models/pokemon_about_model.dart';
+import 'package:pokedex/api/models/pokemon_base_stats_model.dart';
+import 'package:pokedex/api/models/pokemon_moves_model.dart';
+
+part 'app_state.freezed.dart';
+part 'app_state.g.dart';
+
+@freezed
+class AppState with _$AppState {
+  factory AppState({
+    @JsonKey(name: 'pokemon') List<PokemonModel>? pokemon,
+    @JsonKey(name: 'types') List<PokemonTypeModel>? types,
+  }) = _AppState;
+
+  factory AppState.init() => AppState(pokemon: List.empty());
+
+  factory AppState.fromJson(Map<String, dynamic> json) =>
+      _$AppStateFromJson(json);
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,3 @@
+const pokemonURL = 'https://pokeapi.co/api/v2/pokemon';
+
+const pokemonImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
